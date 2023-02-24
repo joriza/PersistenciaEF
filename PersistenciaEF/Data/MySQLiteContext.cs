dbd73e6175cb6272ca12de91db2ad60b539b3bf8
Jorge Javier Izaguirre <jizaguirre@huenei.com>
2023-02-24 10:33:20 -0300

    230224-1033

---
+++ PersistenciaEF/Data/MySQLiteContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PersistenciaEF.Models;
+
+namespace PersistenciaEF.Data
+{
+    public class MySQLiteContext : DbContext
+    {
+        public MySQLiteContext (DbContextOptions<MySQLiteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PersistenciaEF.Models.Car> Car { get; set; }
+    }
+}
---
+++ PersistenciaEF/Migrations/20230224132337_Initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PersistenciaEF.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Car",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Model = table.Column<string>(type: "TEXT", nullable: true),
+                    Make = table.Column<string>(type: "TEXT", nullable: true),
+                    Color = table.Column<string>(type: "TEXT", nullable: true),
+                    Year = table.Column<int>(type: "INTEGER", nullable: false),
+                    Doors = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Car", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Car");
+        }
+    }
+}
